215e20fb23bcab36123ca960bfdffdf5bcd1e112
alamin.147 <alamin.14780@gmail.com>
2024-06-27 00:40:14 +0600

    update

---
+++ one-to-n-without-loop-gfg.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lld long long int
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define forn for (int i = 0; i < n; i++)
+#define fornj for (int j = 0; j < n; j++)
+
+void print(int N)
+{
+
+    if (N == 0)
+        return;
+    print(N - 1);
+    cout << N << " ";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
+#endif
+    print(5);
+    return 0;
+}
