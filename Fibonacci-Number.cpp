1e342b12c2c91b855e5e27dc564dd5ec1481f415
alamin.147 <alamin.14780@gmail.com>
2024-07-06 21:48:34 +0600

    update

---
+++ Fibonacci-Number.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n == 0)
+            return 0;
+        int c = 0;
+        int a0 = 0;
+        int a1 = 1;
+        while (1)
+        {
+            int s = a0 + a1;
+            c++;
+            if (c == n)
+            {
+                return a1;
+            }
+            a0 = a1;
+            a1 = s;
+        }
+    }
+};
+
+int main()
+{
+
+    Solution s;
+    int f = s.fib(0);
+    cout << f << endl;
+    //  f = s.fib(3);
+    // cout << f << endl;
+    //  f = s.fib(5);
+    // cout << f << endl;
+}
