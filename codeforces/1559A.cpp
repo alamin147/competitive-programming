9877acc8dcc1876c3e6c24cbed5aab46d25996a1
alamin147 <alamin.14780@gmail.com>
2025-05-15 22:20:18 +0600

    update

---
+++ codeforces/1559A.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+
+void alfa()
+{
+    int n;
+    cin>>n;
+    unordered_set<ll>s;
+    fn(0,n,1)
+    {
+        ll tt;
+        cin>>tt;
+        s.insert(tt);
+    }
+    ll ans=*s.begin();
+
+    for(auto i:s)
+    ans=ans&i;
+    prn(ans);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
