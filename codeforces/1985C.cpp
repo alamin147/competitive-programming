5c5b5e1d9ac9cc085e97c6c1899255cd1d4084cd
alamin147 <alamin.14780@gmail.com>
2025-05-17 22:48:35 +0600

    update

---
+++ codeforces/1985C.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+
+void alfa()
+{
+     int n;
+        cin >> n;
+        ll a[n];
+        fn(0,n,1) cin >> a[i];
+
+        ll s = 0;
+        ll c = 0;
+        ll maxi = 0;
+        fn(0,n,1)
+        {
+            s += a[i];
+            maxi = max(maxi, a[i]);
+            if (maxi * 2 == s)
+                c++;
+        }
+        cout << c << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
