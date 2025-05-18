0d4daffb29cabf669fbe8e4de511dadf1fd2d436
alamin147 <alamin.14780@gmail.com>
2025-05-18 22:41:14 +0600

    update

---
+++ codeforces/1097B.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+
+void alfa()
+{
+    int n;
+    cin >> n;
+    vc(a,n,int);
+    fn(0,n,1) {
+      cin >> a[i];
+   }
+   bool err = true;
+   for (int mask = 0;mask < (1 << n);mask++) {
+      int sum = 0;
+      for (int k = 0;k < n;k++) {
+         if ((mask >> k) & 1) {
+            sum += a[k];
+         }
+         else {
+            sum -= a[k];
+         }
+      }
+
+      if (sum % 360 == 0) {
+         err = false;
+         break;
+      }
+   }
+
+   if (!err) {
+      py;
+   }
+   else {
+      pn;
+   }
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
