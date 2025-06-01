3bf10da210c399ae4ef0b5a6ff0523e61837e2db
alamin147 <alamin.14780@gmail.com>
2025-06-01 23:41:56 +0600

    update

--- random/a.cpp
+++ random/a.cpp
@@ -3,23 +3,35 @@ using namespace std;
 #define ll long long
 #define py cout<<"YES"<<endl
 #define pn cout<<"NO"<<endl
-#define fn for(int i=0;i<n;i++)
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
 #define s(a) sort(a.begin(),a.end())
 #define prn(c) cout << c <<"\n"
 #define cc cout <<"\n"
-#define pr(a,b) cout << a <<" "<<b
-#define pra(a) cout << a <<" "
-#define vc(v,n) vector<int>v(n)
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
 #define elif else if
-bool validIndex(int i,int n){
-    return i>=0 && i<n;
-}
+bool validIndex(int i, int n);
+bool isEven(int n);
 
 void alfa()
 {
-    int a,b;
-    cin>>a>>b;
-    prn(7-a-b);
+    int n;
+    cin>>n;
+    vc(v,n ,int);
+    fn(0,n,1)
+    {
+        cin>>v[i];
+    }
+    s(v);
+    int c=0;
+    ll ans=0;
+    for(auto i:v)
+    {
+        ans+=abs(c-i);
+        c++;
+    }
+    prn(ans);
 }
 
 int main()
@@ -27,12 +39,20 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-//     int t;
-//     cin>>t;
-//     while(t--)
+    int t;
+    cin>>t;
+    while(t--)
         alfa();
 
-//  #ifndef ONLINE_JUDGE
-//             cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
-//         #endif
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
 }
