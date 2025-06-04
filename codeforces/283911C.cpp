d9bec4f6dabc496cc39288725ebe8ad328648d3c
alamin147 <alamin.14780@gmail.com>
2025-06-05 00:00:20 +0600

    update

---
+++ codeforces/283911C.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define piza ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a,ll b);
+ll lcm(ll a,ll b);
+bool isPrime(ll n);
+
+void alfa()
+{
+    int n,k;
+    cin>>n>>k;
+
+    vc(v,n,ll);
+    fn(0,n,1)
+    cin>>v[i];
+
+   while(k--)
+   {
+        ll q;
+        cin>>q;
+        int l=0,r=n-1;
+        ll ans=0;
+        bool err=true;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(v[mid]>=q)
+            {
+                ans=mid;
+                r=mid-1;
+                err=false;
+            }
+            else
+            l=mid+1;
+        }
+        if(err)
+        prn((n+1));
+        else
+        prn((ans+1));
+   }
+}
+
+int main()
+{
+    piza
+    int t;
+
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
+ll gcd(ll a,ll b){
+return __gcd(a,b);
+}
+ll lcm(ll a,ll b){
+return ((a/__gcd(a,b))*b);
+}
+bool isPrime(ll n){
+if(n<=1)return false;
+if(n<=3)return true;
+if(n%2==0||n%3==0)return false;
+for(ll i=5;i*i<=n;i+=6){
+if(n%i==0||n%(i+2)==0)return false;
+}
+return true;
+}
