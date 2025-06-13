e13b48ff13c19c75f256139baef49ff178dbdaae
alamin147 <alamin.14780@gmail.com>
2025-06-13 22:10:11 +0600

    update

---
+++ cses/1623.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define piza ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a,ll b);
+ll lcm(ll a,ll b);
+bool isPrime(ll n);
+
+void alfa()
+{
+    int n;
+    cin>>n;
+
+    ll total=0;
+    vc(v,n,ll);
+    fn(0,n,1)
+    {
+        cin>>v[i];
+        total+=v[i];
+    }
+
+    ll dif=total;
+
+
+    for(int j=0;j<(1<<n);j++)
+    {
+        ll sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(j&(1<<i))
+            sum+=v[i];
+        }
+        ll s2=total-sum;
+
+        dif=min(abs(s2-sum),dif);
+    }
+    prn(dif);
+
+}
+
+int main()
+{
+    piza
+
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
+ll gcd(ll a,ll b){
+return __gcd(a,b);
+}
+ll lcm(ll a,ll b){
+return ((a/__gcd(a,b))*b);
+}
+bool isPrime(ll n){
+if(n<=1)return false;
+if(n<=3)return true;
+if(n%2==0||n%3==0)return false;
+for(ll i=5;i*i<=n;i+=6){
+if(n%i==0||n%(i+2)==0)return false;
+}
+return true;
+}
