824cbd0dd5d9def626b67aa89ce4417f39e9baf2
alamin147 <alamin.14780@gmail.com>
2025-06-19 18:27:02 +0600

    update

---
+++ codeforces/1840D.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define piza ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a,ll b);
+ll lcm(ll a,ll b);
+bool isPrime(ll n);
+
+void alfa()
+{
+    int n;
+      cin >> n;
+      vector<int> a(n);
+      for (int i = 0;i < n;i++) {
+         cin >> a[i];
+      }
+
+      sort(a.begin(), a.end());
+
+      auto ok = [&](int time) {
+         int cnt = 1, l = 0;
+         for (int r = 0;r < n;r++) {
+            if ((a[r] - a[l]) > 2 * time) {
+               l = r;
+               cnt++;
+            }
+         }
+         return cnt <= 3;
+         };
+
+      int l = 0, r = 1e9, mid, ans;
+      while (l <= r) {
+         mid = l + (r - l) / 2;
+         if (ok(mid)) {
+            ans = mid;
+            r = mid - 1;
+         }
+         else {
+            l = mid + 1;
+         }
+      }
+
+      cout << ans << '\n';
+}
+
+int main()
+{
+    piza
+    int t;
+    cin>>t;
+    while(t--)
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
+ll gcd(ll a,ll b){
+return __gcd(a,b);
+}
+ll lcm(ll a,ll b){
+return ((a/__gcd(a,b))*b);
+}
+bool isPrime(ll n){
+if(n<=1)return false;
+if(n<=3)return true;
+if(n%2==0||n%3==0)return false;
+for(ll i=5;i*i<=n;i+=6){
+if(n%i==0||n%(i+2)==0)return false;
+}
+return true;
+}
