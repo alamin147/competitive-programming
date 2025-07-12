38c74f8680728afda28c701d6b3b8ab2c3064574
alamin147 <alamin.14780@gmail.com>
2025-07-12 23:42:15 +0600

    update

---
+++ codeforces/276C.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define piza                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fn(s, e, in) for (int i = s; i < e; i += in)
+#define s(a) sort(a.begin(), a.end())
+#define prn(c) cout << c << "\n"
+#define cc cout << "\n"
+#define pab(a, b) cout << a << " " << b
+#define pa(a) cout << a << " "
+#define vc(v, n, l) vector<l> v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a, ll b);
+ll lcm(ll a, ll b);
+bool isPrime(ll n);
+
+void alfa()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> d(n + 1);
+    for (int i = 0; i < q; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        l--, r--;
+        d[l]++;
+        d[r + 1]--;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        d[i] = d[i - 1] + d[i];
+    }
+
+    sort(a.rbegin(), a.rend());
+    sort(d.rbegin(), d.rend());
+
+    long long ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        ans += (1LL * a[i] * d[i]);
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    piza int t;
+
+        alfa();
+
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+}
+
+bool validIndex(int i, int n)
+{
+    return i >= 0 && i < n;
+}
+bool isEven(int n)
+{
+    return n % 2 == 0;
+}
+ll gcd(ll a, ll b)
+{
+    return __gcd(a, b);
+}
+ll lcm(ll a, ll b)
+{
+    return ((a / __gcd(a, b)) * b);
+}
+bool isPrime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (ll i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
