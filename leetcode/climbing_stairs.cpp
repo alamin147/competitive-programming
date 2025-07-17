d3f5809f8a3103ea3cc7733bf01fd154f05c37b6
alamin147 <alamin.14780@gmail.com>
2025-07-17 19:48:00 +0600

    update

---
+++ leetcode/climbing_stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return climb(n, memo);
+    }
+
+private:
+    int climb(int n, vector<int>& memo) {
+        if (n == 0 || n == 1)
+            return 1;
+        if (memo[n] != -1)
+            return memo[n];
+        memo[n] = climb(n - 1, memo) + climb(n - 2, memo);
+        return memo[n];
+    }
+};
