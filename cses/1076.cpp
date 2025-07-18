5ef786b5c12177466b1fd8e425c5d3bffb9db003
alamin147 <alamin.14780@gmail.com>
2025-07-18 22:21:21 +0600

    update

---
+++ cses/1076.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define piza                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fn(s, e, in) for (int i = s; i < e; i += in)
+#define s(a) sort(a.begin(), a.end())
+#define prn(c) cout << c << "\n"
+#define cc cout << "\n"
+#define pab(a, b) cout << a << " " << b
+#define pa(a) cout << a << " "
+#define vc(v, n, l) vector<l> v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a, ll b);
+ll lcm(ll a, ll b);
+bool isPrime(ll n);
+
+void alfa()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int l = 0, r = 0;
+    pbds<pair<int, int>> p;
+
+    while (r < n)
+    {
+        p.insert({a[r], r});
+        if (r - l + 1 == k)
+        {
+            int pos = k / 2;
+            if (k % 2 == 0)
+                pos--;
+
+            auto it = p.find_by_order(pos);
+            cout << it->first << " ";
+            p.erase({a[l], l});
+            l++;
+        }
+        r++;
+    }
+    cout << '\n';
+}
+int main()
+{
+    piza int t;
+
+    alfa();
+
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+}
+
+bool validIndex(int i, int n)
+{
+    return i >= 0 && i < n;
+}
+bool isEven(int n)
+{
+    return n % 2 == 0;
+}
+ll gcd(ll a, ll b)
+{
+    return __gcd(a, b);
+}
+ll lcm(ll a, ll b)
+{
+    return ((a / __gcd(a, b)) * b);
+}
+bool isPrime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (ll i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
