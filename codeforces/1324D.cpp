37a64193c9124093db149d83a54a4ce496766341
alamin147 <alamin.14780@gmail.com>
2025-07-26 23:20:50 +0600

    update

---
+++ codeforces/1324D.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define piza ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a,ll b);
+ll lcm(ll a,ll b);
+bool isPrime(ll n);
+ll num_one_bits(ll n);
+
+void alfa()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0;i < n;i++)
+        cin >> a[i];
+    for (ll j = 0;j < n;j++)
+        cin >> b[j];
+
+    ordered_set os;
+    ll ans = 0;
+    for (ll i = 0;i < n;i++)
+    {
+        ans += os.order_of_key(a[i] - b[i]);
+        os.insert(b[i] - a[i]);
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    piza
+
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
+ll gcd(ll a,ll b){
+return __gcd(a,b);
+}
+ll lcm(ll a,ll b){
+return ((a/__gcd(a,b))*b);
+}
+ll num_one_bits(ll n){
+return __builtin_popcountll(n);
+}
+bool isPrime(ll n){
+if(n<=1)return false;
+if(n<=3)return true;
+if(n%2==0||n%3==0)return false;
+for(ll i=5;i*i<=n;i+=6){
+if(n%i==0||n%(i+2)==0)return false;
+}
+return true;
+}
