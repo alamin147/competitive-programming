5f5780c320ee5306ae3254441e7701678d3d0d4e
alamin147 <alamin.14780@gmail.com>
2025-07-27 23:06:18 +0600

    update

---
+++ codeforces/2128A.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define piza ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define s(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a,ll b);
+ll lcm(ll a,ll b);
+bool isPrime(ll n);
+ll num_one_bits(ll n);
+
+void alfa()
+{
+    ll n,m;
+    cin>>n>>m;
+    vc(v,n,ll);
+    fn(0,n,1)
+    cin>>v[i];
+    s(v);
+
+    ll c=0;
+    int k=0;
+    while(!v.empty())
+    {
+        ll bi;
+        if(k<63)
+        bi=m>>k;
+        else
+        bi=0;
+
+        auto it=upper_bound(v.begin(),v.end(),bi);
+        if(it!=v.begin())
+        {
+            v.erase(prev(it));
+        }
+        else
+        {
+            c++;
+            v.pop_back();
+        }
+        k++;
+    }
+    prn(c);
+}
+
+int main()
+{
+    piza
+    int t;
+    cin>>t;
+    while(t--)
+        alfa();
+
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
+ll gcd(ll a,ll b){
+return __gcd(a,b);
+}
+ll lcm(ll a,ll b){
+return ((a/__gcd(a,b))*b);
+}
+ll num_one_bits(ll n){
+return __builtin_popcountll(n);
+}
+bool isPrime(ll n){
+if(n<=1)return false;
+if(n<=3)return true;
+if(n%2==0||n%3==0)return false;
+for(ll i=5;i*i<=n;i+=6){
+if(n%i==0||n%(i+2)==0)return false;
+}
+return true;
+}
