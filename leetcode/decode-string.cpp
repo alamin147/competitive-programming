81548cfa4d2d81c590cdd22ea53236b9e74f64ba
alamin147 <alamin.14780@gmail.com>
2025-08-15 23:11:19 +0600

    update

---
+++ leetcode/decode-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> counts;
+        stack<string> resultStack;
+        string result = "";
+        int i = 0;
+
+        while (i < s.length()) {
+            if (isdigit(s[i])) {
+                int count = 0;
+                while (isdigit(s[i])) {
+                    count = count * 10 + (s[i] - '0');
+                    i++;
+                }
+                counts.push(count);
+            } else if (s[i] == '[') {
+                resultStack.push(result);
+                result = "";
+                i++;
+            } else if (s[i] == ']') {
+                string temp = resultStack.top(); resultStack.pop();
+                int repeat = counts.top(); counts.pop();
+                while (repeat--) temp += result;
+                result = temp;
+                i++;
+            } else {
+                result += s[i];
+                i++;
+            }
+        }
+
+        return result;
+    }
+};
