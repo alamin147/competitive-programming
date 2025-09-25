2b8ebe6cb581ce5837d0b4f51452ed9c0159c9e0
alamin147 <alamin.14780@gmail.com>
2025-09-25 12:39:56 +0600

    updated

---
+++ leetcode/maximum-containers-on-a-ship.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxContainers(int n, int w, int maxWeight) {
+      return min((maxWeight/w),(n*n));
+    }
+};
+
