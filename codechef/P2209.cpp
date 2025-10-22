09790f5e365a89612c9b458e3eef528538e1b62c
alamin147 <alamin.14780@gmail.com>
2025-10-22 21:15:53 +0600

    update

---
+++ codechef/P2209.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define piza ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fn(s,e,in) for(int i=s;i<e;i+=in)
+#define ss(a) sort(a.begin(),a.end())
+#define prn(c) cout << c <<"\n"
+#define cc cout <<"\n"
+#define pab(a,b) cout << a <<" "<<b
+#define pa(a) cout << a <<" "
+#define vc(v,n,l) vector<l>v(n)
+#define elif else if
+bool validIndex(int i, int n);
+bool isEven(int n);
+ll gcd(ll a,ll b);
+ll lcm(ll a,ll b);
+bool isPrime(ll n);
+ll num_one_bits(ll n);
+
+void alfa()
+{
+    int x,y;
+    cin>>x>>y;
+    vector<int>v;
+    int i=1;
+    while(true)
+    {
+        if(i*x>y)
+            break;
+        v.push_back(i*x);
+        i++;
+    }
+    int e=0,o=0;
+    for(auto i:v)
+    {
+        if(i%2==0)
+            e+=i;
+        else o+=i;
+    }
+    if(e>=o)
+        py;
+    else pn;
+
+}
+
+int main()
+{
+    piza
+    int tt;
+    cin>>tt;
+    while(tt--)
+        alfa();
+ #ifndef ONLINE_JUDGE
+            cerr<<"Time: "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+        #endif
+}
+
+bool validIndex(int i,int n){
+    return i>=0 && i<n;
+}
+bool isEven(int n){
+    return n%2==0;
+}
+ll gcd(ll a,ll b){
+return __gcd(a,b);
+}
+ll lcm(ll a,ll b){
+return ((a/__gcd(a,b))*b);
+}
+ll num_one_bits(ll n){
+return __builtin_popcountll(n);
+}
+bool isPrime(ll n){
+if(n<=1)return false;
+if(n<=3)return true;
+if(n%2==0||n%3==0)return false;
+for(ll i=5;i*i<=n;i+=6){
+if(n%i==0||n%(i+2)==0)return false;
+}
+return true;
+}
+
